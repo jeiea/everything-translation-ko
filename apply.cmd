bbbcb7397328695cfc80b33b3a09e890e0b75500
jeiea <solum5013@gmail.com>
2026-03-15 03:29:24 +0900

    chore: require admin for copy in apply.cmd

--- apply.cmd
+++ apply.cmd
@@ -1,7 +1,23 @@
+@echo off
+net session >nul 2>&1 || (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+set "E=C:\Program Files\Everything"
+set "E15=C:\Program Files\Everything 1.5a"
+
 @echo on
 cd /D "%~dp0"
 makelng
-"C:\Program Files\Everything\Everything.exe" -exit
-copy /y "%~dp0Everything.lng" "C:\Program Files\Everything\Everything.lng"
-start "" "C:\Program Files\Everything\Everything.exe" -startup
+if exist "%E%\Everything.exe" (
+    "%E%\Everything.exe" -exit
+    copy /y "%~dp0Everything.lng" "%E%\Everything.lng"
+    start "" "%E%\Everything.exe" -startup
+)
+if exist "%E15%\Everything.exe" (
+    "%E15%\Everything.exe" -exit
+    copy /y "%~dp0Everything.lng" "%E15%\Everything.lng"
+    start "" "%E15%\Everything.exe" -startup
+)
 pause
