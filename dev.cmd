9777eb3167c21e0b9b01e88ec976beadf1e31b21
jeiea <solum5013@gmail.com>
2026-03-15 03:11:01 +0900

    chore: unify winmerge setting to script

--- dev.cmd
+++ dev.cmd
@@ -1,4 +1,4 @@
-start "" "C:\Program Files\WinMerge\WinMergeU.exe" /s "%~dp0program.WinMerge"
-start "" "C:\Program Files\WinMerge\WinMergeU.exe" /s "%~dp0makevoidtools\website.WinMerge"
+start "" "C:\Program Files\WinMerge\WinMergeU.exe" /s "Template.txt" "Korean.txt"
+start "" "C:\Program Files\WinMerge\WinMergeU.exe" /s "makevoidtools\en-us\" "makevoidtools\ko-kr\"
 @REM start watch "makevoidtools.exe build http://localhost:8080" ko-kr
 @REM live-server --entry-file=index.html --mount=/:./build --open=/ko-kr
